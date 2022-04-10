7e333de68ff7fbd529175d559182022e13cdd844
Ratmir <rklimow409@gmail.com>
2022-04-10 09:31:16 +0300

    2

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize(
-    process.env.DB_NAME || 'task_4',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || '123',
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || '5432',
-    }
-)
+if (process.env.NODE_ENV === 'development') {
+    module.exports = new Sequelize(
+        process.env.DB_NAME || 'task_4',
+        process.env.DB_USER || 'postgres',
+        process.env.DB_PASSWORD || '123',
+        {
+            dialect: 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || '5432',
+        }
+    )
+}
+if (process.env.NODE_ENV === 'production') {
+    module.exports = new Sequelize(
+        process.env.DATABASE_URL
+    )
+}
